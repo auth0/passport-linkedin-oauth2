4920b73ed8cbaf57ffb4337f97a1a6d3a6a5cda4
Eugenio Pace <eugenio.pace@gmail.com>
2013-03-30 15:15:26 -0700

    fixed scope in userprofile

---
+++ test/strategy.tests.js
@@ -0,0 +1,46 @@
+var Strategy = require('../lib/index').OAuth2Strategy;
+
+describe.only('LinkedIn Stratgey', function () {
+
+  it('init with basic profile', function (done) {
+      
+    var options = {
+        clientID: "clientId",
+        clientSecret: "clientSecret"
+      };
+
+    var st = new Strategy(options, function(){});
+    st.name.should.eql("linkedin");
+    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url)');
+    done();
+    });
+  
+  it('init with email scope', function (done) {
+      
+    var options = {
+        clientID: "clientId",
+        clientSecret: "clientSecret",
+        scope: ['r_emailaddress']
+      };
+
+    var st = new Strategy(options, function(){});
+    st.name.should.eql("linkedin");
+    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url,email-address)');
+    done();
+    });
+
+
+  it('init with profile and email scope', function (done) {
+      
+    var options = {
+        clientID: "clientId",
+        clientSecret: "clientSecret",
+        scope: ['r_basicprofile', 'r_emailaddress']
+      };
+
+    var st = new Strategy(options, function(){});
+    st.name.should.eql("linkedin");
+    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url,email-address)');
+    done();
+    });
+});
