bc955f9ef7add22ad3b4a7074dec368373f7c343
Josh Schell <jschell12@gmail.com>
2013-09-29 09:30:49 -0400

    Custom profile fields
    Giving the user the ability to specify profile fields and added
    r_fullprofile.

--- test/strategy.tests.js
+++ test/strategy.tests.js
@@ -1,3 +1,5 @@
+var assert = require("assert");
+var should = require("should");
 var Strategy = require('../lib/index').OAuth2Strategy;
 
 describe.only('LinkedIn Strategy', function () {
@@ -25,8 +27,30 @@ describe.only('LinkedIn Strategy', function () {
 
     var st = new Strategy(options, function(){});
     st.name.should.eql("linkedin");
-    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url,formatted-name,email-address)');
-    done();
+    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,'+ 
+      'first-name,'+ 
+      'last-name,'+ 
+      'picture-url,'+ 
+      'formatted-name,'+
+      'maiden-name,'+
+      'phonetic-first-name,'+
+      'phonetic-last-name,'+
+      'formatted-phonetic-name,'+
+      'headline,'+
+      'location:(name,country:(code)),'+
+      'industry,'+
+      'distance,'+
+      'relation-to-viewer:(distance,connections),'+
+      'current-share,'+
+      'num-connections,'+
+      'num-connections-capped,'+
+      'summary,'+
+      'specialties,'+
+      'positions,'+
+      'site-standard-profile-request,'+
+      'api-standard-profile-request:(headers,url),'+
+      'public-profile-url,email-address)');
+        done();
     });
 
 
@@ -40,7 +64,73 @@ describe.only('LinkedIn Strategy', function () {
 
     var st = new Strategy(options, function(){});
     st.name.should.eql("linkedin");
-    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url,formatted-name,email-address)');
+     st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,'+ 
+      'first-name,'+ 
+      'last-name,'+ 
+      'picture-url,'+ 
+      'formatted-name,'+
+      'maiden-name,'+
+      'phonetic-first-name,'+
+      'phonetic-last-name,'+
+      'formatted-phonetic-name,'+
+      'headline,'+
+      'location:(name,country:(code)),'+
+      'industry,'+
+      'distance,'+
+      'relation-to-viewer:(distance,connections),'+
+      'current-share,'+
+      'num-connections,'+
+      'num-connections-capped,'+
+      'summary,'+
+      'specialties,'+
+      'positions,'+
+      'site-standard-profile-request,'+
+      'api-standard-profile-request:(headers,url),'+
+      'public-profile-url,email-address)');
+     done();
+    });
+
+
+  it('init with custom parameters', function (done) {
+      
+    var options = {
+        clientID: "clientId",
+        clientSecret: "clientSecret",
+        scope: {'r_basicprofile': [ 'id', 'first-name', 'last-name']}
+      };
+
+    var st = new Strategy(options, function(){});
+    st.name.should.eql("linkedin");
+    st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name)');
     done();
+    });
+
+    it('init with r_fullprofile custom parameters', function (done) {
+      
+        var options = {
+            clientID: "clientId",
+            clientSecret: "clientSecret",
+            scope: {'r_fullprofile': [ 'id', 'first-name', 'last-name','first-proposal-comments']}
+          };
+
+        var st = new Strategy(options, function(){});
+        st.name.should.eql("linkedin");
+        st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,first-proposal-comments)');
+        done();
+    });
+
+
+    it('init with r_fullprofile scope used but only r_basicprofile fields specified', function (done) {
+      
+        var options = {
+            clientID: "clientId",
+            clientSecret: "clientSecret",
+            scope: {'r_fullprofile': [ 'id', 'first-name', 'last-name']}
+          };
+
+        var st = new Strategy(options, function(){});
+        st.name.should.eql("linkedin");
+        st.profileUrl.should.eql('https://api.linkedin.com/v1/people/~:(id,first-name,last-name)');
+        done();
     });
 });
