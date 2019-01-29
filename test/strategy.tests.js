c82e626664a199e6e70a57e8ec1082e3f7c4fe24
Sebastián E. Peyrott <sebastian.peyrott@auth0.com>
2019-01-29 18:04:23 -0300

    Some work on unit-tests.

--- test/strategy.tests.js
+++ test/strategy.tests.js
@@ -1,40 +1,58 @@
-var should = require("should");
-var nock = require('nock');
-var Strategy = require('../lib/index').OAuth2Strategy;
+const should = require("should");
+const nock = require('nock');
+const Strategy = require('../lib/index').OAuth2Strategy;
+const liteProfileExample = require('./lite-profile.json');
+const basicProfileExample = require('./basic-profile.json');
 
 nock.disableNetConnect();
 
 describe('LinkedIn Strategy', function () {
+  const origin = 'https://api.linkedin.com';
+
+  const liteProfilePath = '/v2/me?projection=(id%2CfirstName%2ClastName%2CmaidenName%2CprofilePicture(displayImage~%3AplayableStreams))&oauth2_access_token=whatever';
+
+  const emailPath = '/v2/emailAddress?q=members&projection=(elements*(handle~))&oauth2_access_token=whatever';
+
+  const basicProfilePath = '/v2/me?projection=(id%2CfirstName%2ClastName%2CmaidenName%2CprofilePicture(displayImage~%3AplayableStreams)%2CphoneticFirstName%2CphoneticLastName%2Cheadline%2Clocation%2CindustryId%2Csummary%2Cpositions%2CvanityName%2ClastModified)&oauth2_access_token=whatever';
+
   it('sanity check', function (done) {
-    var options = {
+    const options = {
       clientID: "clientId",
       clientSecret: "clientSecret"
     };
-    var st = new Strategy(options, function () { });
+    const st = new Strategy(options, function () { });
 
     st.name.should.eql("linkedin");
-    st.profileUrl.should.eql('https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,maidenName,profilePicture(displayImage~:playableStreams))');
-    st.emailUrl.should.eql('https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))');
+
+    const decodedProfilePath = decodeURIComponent(liteProfilePath).replace('&oauth2_access_token=whatever', '');
+    const decodedEmailPath = decodeURIComponent(emailPath).replace('&oauth2_access_token=whatever', '');
+
+    st.profileUrl.should.eql(`${origin}${decodedProfilePath}`);
+    st.emailUrl.should.eql(`${origin}${decodedEmailPath}`);
 
     done();
   });
 
   describe('userProfile(accessToken, done)', function () {
-    before(function () {
-      nock('https://api.linkedin.com')
-        .get('/v2/me?projection=(id%2CfirstName%2ClastName%2CmaidenName%2CprofilePicture(displayImage~%3AplayableStreams))&oauth2_access_token=whatever')
-        .times(2)
-        .reply(200, require('./lite-profile.json'));
-    });
 
     context('with r_liteprofile scope', function () {
+      beforeEach(function () {
+        this.scope = nock(origin)
+          .get(liteProfilePath)
+          .reply(200, liteProfileExample);
+      });
+
+      afterEach(function () {
+        this.scope.done();
+      });
+
       it('passes id, firstname, lastname and profile picture fields to callback', function (done) {
-        var options = {
+        const options = {
           clientID: "clientId",
           clientSecret: "clientSecret"
         };
 
-        var st = new Strategy(options, function () { });
+        const st = new Strategy(options, function () { });
 
         st.userProfile('whatever', function (err, profile) {
           should.not.exist(err);
@@ -45,26 +63,33 @@ describe('LinkedIn Strategy', function () {
           profile.photos.should.eql([{
             value: 'https://media.licdn.com/dms/image/C4D03AQGsitRwG8U8ZQ/profile-displayphoto-shrink_100_100/0?e=1526940000&v=alpha&t=12345'
           }]);
+
           done();
         });
       });
     });
 
     context('with r_emailaddress scope', function () {
-      before(function () {
-        nock('https://api.linkedin.com')
-          .get('/v2/emailAddress?q=members&projection=(elements*(handle~))&oauth2_access_token=whatever')
-          .reply(200, require('./email-address.json'))
+      beforeEach(function () {
+        this.scope = nock(origin)
+          .get(liteProfilePath)
+          .reply(200, liteProfileExample)
+          .get(emailPath)
+          .reply(200, require('./email-address.json'));
+      });
+
+      afterEach(function() {
+        this.scope.done();
       });
 
       it('passes also email field to callback', function (done) {
-        var options = {
+        const options = {
           clientID: "clientId",
           clientSecret: "clientSecret",
           scope: ['r_liteprofile', 'r_emailaddress']
         };
 
-        var st = new Strategy(options, function () { });
+        const st = new Strategy(options, function () { });
 
         st.userProfile('whatever', function (err, profile) {
           should.not.exist(err);
@@ -79,21 +104,80 @@ describe('LinkedIn Strategy', function () {
       });
     });
 
+    context('with r_basicprofile scope', function () {
+      beforeEach(function () {
+        this.scope = nock(origin)
+          .get(basicProfilePath)
+          .reply(200, basicProfileExample);
+      });
+
+      afterEach(function () {
+        this.scope.done();
+      });
+
+      it('passes all basic profile fields to callback', function (done) {
+        const options = {
+          clientID: "clientId",
+          clientSecret: "clientSecret",
+          scope: ['r_basicprofile']
+        };
+
+        const st = new Strategy(options, function () { });
+
+        st.userProfile('whatever', function (err, profile) {
+          should.not.exist(err);
+          profile.id.should.eql('REDACTED');
+          profile.name.givenName.should.eql('Tina');
+          profile.name.familyName.should.eql('Belcher');
+          profile.displayName.should.eql('Tina Belcher');
+          profile.photos.should.eql([{
+            value: 'https://media.licdn.com/dms/image/C4D03AQGsitRwG8U8ZQ/profile-displayphoto-shrink_100_100/0?e=1526940000&v=alpha&t=12345'
+          }]);
+
+          // TODO: add basic profile fields once we have a valid example.
+
+          done();
+        });
+      });
+
+      it('should still request basic profile fields when used at the same time as r_liteprofile', function (done) {
+        const options = {
+          clientID: "clientId",
+          clientSecret: "clientSecret",
+          scope: ['r_liteprofile', 'r_basicprofile']
+        };
+
+        const st = new Strategy(options, function () { });
+
+        st.userProfile('whatever', function (err, profile) {
+
+          // TODO: check one basic profile field, other checks performed
+          // by nock in afterEach block.
+
+          done(err);
+        });
+      });
+    });
+
     context('when error occurs', function () {
-      before(function () {
-        nock('https://api.linkedin.com')
-          .get('/v2/.*')
-          .reply(500)
+      beforeEach(function () {
+        this.scope = nock(origin)
+          .get(liteProfilePath)
+          .reply(500);
+      });
+
+      afterEach(function () {
+        this.scope.done();
       });
 
       it('passes error to callback', function (done) {
-        var options = {
+        const options = {
           clientID: "clientId",
           clientSecret: "clientSecret",
           scope: ['r_liteprofile', 'r_emailaddress']
         };
 
-        var st = new Strategy(options, function () { });
+        const st = new Strategy(options, function () { });
 
         st.userProfile('whatever', function (err, profile) {
           should.exist(err);
